9304b74c262cd180754659db3d33381b2d6738f0
lprevost <prevost.louis7@gmail.com>
2025-12-02 18:08:15 +0100

    Remove build folder from tracking

--- build/CMakeFiles/CGNUM.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/CGNUM"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/CGNUM.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- build/CMakeFiles/GNUM.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/GNUM"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/GNUM.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
